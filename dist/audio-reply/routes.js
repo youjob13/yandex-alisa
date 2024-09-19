4ccfd17e79874f7a490d4e6cc6b499bac1b0d647
Danil Rodionov <Danil_Rodionov@epam.com>
2024-09-19 12:23:21 +0400

    feat: add logger

--- dist/audio-reply/routes.js
+++ dist/audio-reply/routes.js
@@ -1,7 +1,8 @@
 import { AudioReplyService } from '@myalisa/audio-reply';
 export const createRouter = (app) => {
     const audioReplyService = new AudioReplyService();
-    app.post('/', async () => {
+    app.post('/', async (request) => {
+        app.log.info('[Request]', request);
         await audioReplyService.play();
         return { hello: 1 };
     });
--- src/audio-reply/routes.ts
+++ src/audio-reply/routes.ts
@@ -4,7 +4,8 @@ import { FastifyInstance } from 'fastify'
 export const createRouter = (app: FastifyInstance) => {
     const audioReplyService = new AudioReplyService()
 
-    app.post('/', async () => {
+    app.post('/', async (request) => {
+        app.log.info('[Request]', request)
         await audioReplyService.play()
         return { hello: 1 }
     })
